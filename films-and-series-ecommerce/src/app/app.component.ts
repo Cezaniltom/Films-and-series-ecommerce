81a20f8fec0bd009570bf6edaf7e08461644e7b4
cezaniltom <cezaniltom@outlook.com>
2024-11-26 10:11:18 -0300

    Componente card criado

--- films-and-series-ecommerce/src/app/app.component.ts
+++ films-and-series-ecommerce/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './shared/header/header.component';
+import { CardFaSComponent } from './shared/card-fa-s/card-fa-s.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CardFaSComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
